2228a615e0c5cb3e3b9747b9f19f9164b9c7bc6d
AlexZHU <zrl920825@hotmail.com>
2018-01-21 00:02:20 +0800

    找到原因了,请求下来的页面和浏览器页面不同

--- daidaihong_taobao/crawler.py
+++ daidaihong_taobao/crawler.py
@@ -5,27 +5,24 @@ import urllib
 import urllib.request
 import asyncio
 import re
+import requests
 from bs4 import BeautifulSoup
 
 def requestTaoBao(url):
-    user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+    user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.22 Safari/537.36 SE 2.X MetaSr 1.0'
     req = urllib.request.Request(url, data=None, headers={
-        'User-Agent': user_agent
+        'user-agent': user_agent
     })
     response = urllib.request.urlopen(req)
     return response.read()
     
 def getShopAllGoods(content):
     #解析
-    soup = BeautifulSoup(content, "lxml")
-    # print(soup.prettify())
-    reg = re.compile('class="item')
-    print(reg)
-    item = soup.find_all(reg)
-    print(item)
+    soup = BeautifulSoup(content, "lxml", from_encoding='utf-8')
+    more_btn_url = soup.find(name='a', class_='show-more border-radius hotsell_desc')['href']
+    soup2 = BeautifulSoup(requestTaoBao('http:' + more_btn_url), 'lxml', from_encoding=None)
+    print(soup2.prettify)
 
-shop_url = 'https://shop458578821.taobao.com/search.htm?spm=a1z10.1-c-s.0.0.361cee58SIdQEK&search=y&orderType=hotsell_desc'
+shop_url = 'https://shop458578821.taobao.com/search.htm'
 getShopAllGoods(requestTaoBao(shop_url))
-
-
-
+# print(requests.get(shop_url).text)
